10a512539683a23ab490c28cb48d8b95ee046f50
Natan Santolo <natan.santolo@gmail.com>
2016-06-23 11:16:11 +0200

    Added number to side bar

--- app/src/PlacesConstant.js
+++ app/src/PlacesConstant.js
@@ -1,26 +1,5 @@
 angular.module('App').constant('places', [
     {
-        "id": "ChIJaTMo1sEJxkcRfjuQODB7BK8",
-        "color": "#E37F00",
-        "label": "Bloemenmarkt",
-        "category": "Markets",
-        "content": [
-            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim.",
-            "Eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue.",
-            "Id quod mazim placerat facer, possim assum typi non habent claritatem insitam est usus legentis in. Consectetuer adipiscing elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat! Te feugait nulla facilisi nam liber tempor cum soluta. Consequat duis autem vel eum iriure dolor in hendrerit in vulputate velit."
-        ],
-        "author": {
-            "name": "Marie Bennett",
-            "role": "Architect",
-            "picture": "/assets/img/avatars/MarieBennett.jpg"
-        },
-        "media": {
-            "x1": "/assets/img/x1/bloemenmarkt.jpg",
-            "x2": "/assets/img/x2/bloemenmarkt.jpg",
-            "x3": "/assets/img/x3/bloemenmarkt.jpg"
-        }
-    },
-    {
         "id": "ChIJSRE-IcUJxkcRCltjPmVdmtQ",
         "color": "#1D8800",
         "label": "Anne Frank Huis",
@@ -39,26 +18,6 @@ angular.module('App').constant('places', [
             "x1": "/assets/img/x1/annefrank.jpg",
             "x2": "/assets/img/x2/annefrank.jpg",
             "x3": "/assets/img/x3/annefrank.jpg"
-        }
-    },
-    {
-        "id": "ChIJ3UYLU3XixUcRlxW4bSCFqdQ",
-        "color": "#c5a881",
-        "label": "Foodhallen",
-        "website": "http://www.foodhallen.nl",
-        "category": "Restaurants",
-        "content": [
-            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim."
-        ],
-        "author": {
-            "name": "Marie Bennett",
-            "role": "Architect",
-            "picture": "/assets/img/avatars/MarieBennett.jpg"
-        },
-        "media": {
-            "x1": "/assets/img/x1/bloemenmarkt.jpg",
-            "x2": "/assets/img/x2/bloemenmarkt.jpg",
-            "x3": "/assets/img/x3/bloemenmarkt.jpg"
         }
     },
     {
@@ -82,34 +41,13 @@ angular.module('App').constant('places', [
         }
     },
     {
-        "id": "ChIJIyPEUu8JxkcRTmRcuMxC9yc",
-        "color": "#2688FF",
-        "label": "Hi-Tec Store",
-        "website": "http://www.hi-tec.amsterdam",
+        "id": "ChIJ3UYLU3XixUcRlxW4bSCFqdQ",
+        "color": "#c5a881",
+        "label": "Foodhallen",
+        "website": "http://www.foodhallen.nl",
+        "category": "Restaurants",
         "content": [
-            "Dolore te feugait nulla facilisi nam liber tempor cum, soluta nobis eleifend option. Facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod.",
-            "Putamus parum claram anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Legentis in iis: qui ii legunt saepius claritas. Luptatum zzril delenit augue duis congue nihil imperdiet doming.",
-            "Sequitur mutationem consuetudium lectorum mirum est notare quam littera gothica quam nunc? Qui eodem modo; typi qui nunc nobis videntur parum clari fiant sollemnes in?"
-        ],
-        "author": {
-            "name": "Andrea Clarke",
-            "role": "Entrepreneur",
-            "picture": "/assets/img/avatars/AndreaClarke.jpg"
-        },
-        "media": {
-            "x1": "/assets/img/x1/hitecstore.jpg",
-            "x2": "/assets/img/x2/hitecstore.jpg",
-            "x3": "/assets/img/x3/hitecstore.jpg"
-        }
-    },
-    {
-        "id": "ChIJezIPCY8JxkcRgdqv8FrYp8U",
-        "color": "#2A6E30",
-        "label": "Juice Brothers",
-        "website": "http://www.juicebro.com",
-        "category": "Healthy Bars",
-        "content": [
-            "JuiceBrothers their goal is to create quality cold-pressed juice that is nutritious and delicious as hell."
+            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim."
         ],
         "author": {
             "name": "Marie Bennett",
@@ -117,9 +55,30 @@ angular.module('App').constant('places', [
             "picture": "/assets/img/avatars/MarieBennett.jpg"
         },
         "media": {
-            "x1": "/assets/img/x1/juicebrothers.jpg",
-            "x2": "/assets/img/x2/juicebrothers.jpg",
-            "x3": "/assets/img/x3/juicebrothers.jpg"
+            "x1": "/assets/img/x1/bloemenmarkt.jpg",
+            "x2": "/assets/img/x2/bloemenmarkt.jpg",
+            "x3": "/assets/img/x3/bloemenmarkt.jpg"
+        }
+    },
+    {
+        "id": "ChIJaTMo1sEJxkcRfjuQODB7BK8",
+        "color": "#E37F00",
+        "label": "Bloemenmarkt",
+        "category": "Markets",
+        "content": [
+            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim.",
+            "Eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue.",
+            "Id quod mazim placerat facer, possim assum typi non habent claritatem insitam est usus legentis in. Consectetuer adipiscing elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat! Te feugait nulla facilisi nam liber tempor cum soluta. Consequat duis autem vel eum iriure dolor in hendrerit in vulputate velit."
+        ],
+        "author": {
+            "name": "Marie Bennett",
+            "role": "Architect",
+            "picture": "/assets/img/avatars/MarieBennett.jpg"
+        },
+        "media": {
+            "x1": "/assets/img/x1/bloemenmarkt.jpg",
+            "x2": "/assets/img/x2/bloemenmarkt.jpg",
+            "x3": "/assets/img/x3/bloemenmarkt.jpg"
         }
     },
     {
