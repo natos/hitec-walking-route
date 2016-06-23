af542b61486f0c8e686321865e0517286bd6503a
Natan Santolo <natan.santolo@gmail.com>
2016-06-23 11:29:51 +0200

    added rembrandpark to the list

--- app/src/PlacesConstant.js
+++ app/src/PlacesConstant.js
@@ -21,6 +21,27 @@ angular.module('App').constant('places', [
         }
     },
     {
+        "id": "ChIJaTMo1sEJxkcRfjuQODB7BK8",
+        "color": "#E37F00",
+        "label": "Bloemenmarkt",
+        "category": "Markets",
+        "content": [
+            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim.",
+            "Eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue.",
+            "Id quod mazim placerat facer, possim assum typi non habent claritatem insitam est usus legentis in. Consectetuer adipiscing elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat! Te feugait nulla facilisi nam liber tempor cum soluta. Consequat duis autem vel eum iriure dolor in hendrerit in vulputate velit."
+        ],
+        "author": {
+            "name": "Marie Bennett",
+            "role": "Architect",
+            "picture": "/assets/img/avatars/MarieBennett.jpg"
+        },
+        "media": {
+            "x1": "/assets/img/x1/bloemenmarkt.jpg",
+            "x2": "/assets/img/x2/bloemenmarkt.jpg",
+            "x3": "/assets/img/x3/bloemenmarkt.jpg"
+        }
+    },
+    {
         "id": "Ei5SdXlzZGFlbGthZGUgMTQ5LCAxMDcyIEFSIEFtc3RlcmRhbSwgTmVkZXJsYW5k",
         "color": "#B43E43",
         "label": "Cannibale Royale",
@@ -61,14 +82,34 @@ angular.module('App').constant('places', [
         }
     },
     {
-        "id": "ChIJaTMo1sEJxkcRfjuQODB7BK8",
-        "color": "#E37F00",
-        "label": "Bloemenmarkt",
-        "category": "Markets",
+        "id": "ChIJIyPEUu8JxkcRTmRcuMxC9yc",
+        "color": "#2688FF",
+        "label": "Hi-Tec Store",
+        "website": "http://www.hi-tec.amsterdam",
         "content": [
-            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim.",
-            "Eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue.",
-            "Id quod mazim placerat facer, possim assum typi non habent claritatem insitam est usus legentis in. Consectetuer adipiscing elit sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat! Te feugait nulla facilisi nam liber tempor cum soluta. Consequat duis autem vel eum iriure dolor in hendrerit in vulputate velit."
+            "Dolore te feugait nulla facilisi nam liber tempor cum, soluta nobis eleifend option. Facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod.",
+            "Putamus parum claram anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Legentis in iis: qui ii legunt saepius claritas. Luptatum zzril delenit augue duis congue nihil imperdiet doming.",
+            "Sequitur mutationem consuetudium lectorum mirum est notare quam littera gothica quam nunc? Qui eodem modo; typi qui nunc nobis videntur parum clari fiant sollemnes in?"
+        ],
+        "author": {
+            "name": "Andrea Clarke",
+            "role": "Entrepreneur",
+            "picture": "/assets/img/avatars/AndreaClarke.jpg"
+        },
+        "media": {
+            "x1": "/assets/img/x1/hitecstore.jpg",
+            "x2": "/assets/img/x2/hitecstore.jpg",
+            "x3": "/assets/img/x3/hitecstore.jpg"
+        }
+    },
+    {
+        "id": "ChIJezIPCY8JxkcRgdqv8FrYp8U",
+        "color": "#2A6E30",
+        "label": "Juice Brothers",
+        "website": "http://www.juicebro.com",
+        "category": "Healthy Bars",
+        "content": [
+            "JuiceBrothers their goal is to create quality cold-pressed juice that is nutritious and delicious as hell."
         ],
         "author": {
             "name": "Marie Bennett",
@@ -76,9 +117,9 @@ angular.module('App').constant('places', [
             "picture": "/assets/img/avatars/MarieBennett.jpg"
         },
         "media": {
-            "x1": "/assets/img/x1/bloemenmarkt.jpg",
-            "x2": "/assets/img/x2/bloemenmarkt.jpg",
-            "x3": "/assets/img/x3/bloemenmarkt.jpg"
+            "x1": "/assets/img/x1/juicebrothers.jpg",
+            "x2": "/assets/img/x2/juicebrothers.jpg",
+            "x3": "/assets/img/x3/juicebrothers.jpg"
         }
     },
     {
@@ -100,6 +141,27 @@ angular.module('App').constant('places', [
             "x1": "/assets/img/x1/bloemenmarkt.jpg",
             "x2": "/assets/img/x2/lionnoir.jpg",
             "x3": "/assets/img/x3/lionnoir.jpg"
+        }
+    },
+    {
+        "id": "ChIJ-e1IGhTixUcRy260F0WUXj4",
+        "color": "#1D8800",
+        "label": "Rembrandtpark",
+        "category": "Typical Amsterdam",
+        "content": [
+            "Iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla. Option congue nihil imperdiet doming id quod mazim placerat facer possim.",
+            "Minim veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea! Facilisi nam liber tempor cum soluta nobis eleifend assum typi non habent claritatem insitam est.",
+            "Ea commodo consequat duis autem vel eum iriure dolor in hendrerit? Litterarum formas humanitatis per seacula quarta decima et quinta decima eodem modo typi."
+        ],
+        "author": {
+            "name": "Shannon Banks",
+            "role": "Fashion designer",
+            "picture": "/assets/img/avatars/ShannonBanks.jpg"
+        },
+        "media": {
+            "x1": "/assets/img/x1/annefrank.jpg",
+            "x2": "/assets/img/x2/annefrank.jpg",
+            "x3": "/assets/img/x3/annefrank.jpg"
         }
     }
 ]);
