84f0ac22511bea6a3e4a63431076e5b275b9b294
Natan Santolo <natan.santolo@gmail.com>
2016-06-29 15:46:21 +0200

    Cleaning

--- app/src/DirectionsDirective.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('App')
-  .directive('directions', function() {
-    return {
-      restrict: 'E',
-      templateUrl: './src/Directions.html',
-      controller: 'DirectionsController as directions'
-    };
-  });
