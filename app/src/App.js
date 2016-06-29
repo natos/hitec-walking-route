b884eff90803d010479a61374816dd3c131edfbe
Natan Santolo <natan.santolo@gmail.com>
2016-06-29 15:41:12 +0200

    Cleaning

---
+++ app/src/App.js
@@ -0,0 +1,24 @@
+angular
+  .module('App', ['ngSanitize', 'ngMaterial'])
+  .config(function($mdThemingProvider, $mdIconProvider) {
+
+    $mdIconProvider
+      .icon("directions", "./assets/svg/ic_directions_black_24px.svg" , 24)
+      .icon("walk", "./assets/svg/ic_directions_walk_white_24px.svg" , 24)
+      .icon("walking_blue", "./assets/svg/ic_directions_walk_blue_mod_24px.svg" , 24)
+      .icon("place", "./assets/svg/ic_place_white_24px.svg" , 24)
+      .icon("place_dark", "./assets/svg/ic_place_black_24px.svg" , 24)
+      .icon("close", "./assets/svg/ic_close_white_24px.svg" , 24)
+      .icon("email", "./assets/svg/ic_email_white_24px.svg" , 24)
+      .icon("print", "./assets/svg/ic_print_white_24px.svg" , 24)
+      .icon("refresh", "./assets/svg/ic_refresh_white_24px.svg" , 24)
+      .icon("my_location", "./assets/svg/ic_my_location_white_24px.svg" , 24)
+      .icon("more_vert", "./assets/svg/ic_more_vert_white_24px.svg" , 24)
+      ;
+
+    $mdThemingProvider
+      .theme('default')
+      .primaryPalette('blue')
+      .accentPalette('red');
+
+  });
---
+++ app/src/SelectingPlacesController.js
@@ -0,0 +1,22 @@
+angular
+  .module('App')
+  .controller('SelectingPlacesController', [
+    '$scope',
+    'appModel',
+    // 'markersService',
+    // 'placesService',
+    SelectingPlacesController
+  ]);
+
+
+  /**
+   * SelectingPlaces Controller for Hi-Tec Walking Route
+   * @constructor
+   */
+  function SelectingPlacesController($scope, appModel) {
+    // markersService.getPlaces();
+
+    this.next = function() {
+      $scope.$emit(appModel.events.nextState);
+    }
+  }
