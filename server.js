1aaffe8c0907ccfc710e0dd511d2c19feea3fbbe
Natan Santolo <natan.santolo@gmail.com>
2016-06-29 14:59:26 +0200

    Updated express syntax

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var app = require('express')();
 var server = require('http').Server(app);
 var port = process.env.PORT || 3000;
 
-server.listen(port);
-
+app.set('port', port);
+app.use(express.static(__dirname + '/app'));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/app/index.html');
 });
-app.listen(port, function() {
-  console.log("Walking Route is running: " + app.get('port'))
+app.listen(app.get('port'), function() {
+  console.log("Walking Route is running: " + app.get('port'));
 });
