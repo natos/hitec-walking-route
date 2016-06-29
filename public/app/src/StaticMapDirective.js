ecdc81c750fe825b9fd2d742f11ae5be47ea69ef
Natan Santolo <natan.santolo@gmail.com>
2016-06-29 15:37:09 +0200

    Cleaning

--- public/app/src/StaticMapDirective.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('App')
-  .directive('staticMap', function() {
-    return {
-      restrict: 'E',
-      templateUrl: './src/StaticMap.html',
-      controller: 'StaticMapController as static'
-    };
-  });
