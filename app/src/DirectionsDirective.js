f85c1ba15210e16a9c55202f4b7ba3ae64c9626b
Natan Santolo <natan.santolo@gmail.com>
2016-06-29 15:35:29 +0200

    Squashed 'public/' content from commit c1b5022
    git-subtree-dir: public
    git-subtree-split: c1b50227498dd172c38b6ae1e3eeccea514714f0

---
+++ app/src/DirectionsDirective.js
@@ -0,0 +1,9 @@
+angular
+  .module('App')
+  .directive('directions', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './src/Directions.html',
+      controller: 'DirectionsController as directions'
+    };
+  });
