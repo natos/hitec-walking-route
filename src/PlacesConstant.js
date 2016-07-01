a566a17b27c2b27cfa00bd5bd67f4009409e6e6c
Natan Santolo <natan.santolo@gmail.com>
2016-07-01 16:58:42 +0200

    rebuilding site Fri Jul  1 16:58:42 CEST 2016

--- src/PlacesConstant.js
+++ src/PlacesConstant.js
@@ -1,5 +1,41 @@
 angular.module('App').constant('places', [
     {
+        "id": "Ei5SdXlzZGFlbGthZGUgMTQ5LCAxMDcyIEFSIEFtc3RlcmRhbSwgTmVkZXJsYW5k",
+        "label": "Cannibale Royale",
+        "website": "http://www.cannibaleroyale.nl",
+        "category": "Restaurants",
+        "content": [
+            "Cannibale Royale, brasserie extraordinaire fulfills all needs for anyone who loves good food, lots of meat, exotic beers and the most beautiful wines."
+        ],
+        "author": {
+            "name": "Marie Bennett",
+            "role": "Architect",
+            "picture": "assets/img/author/MarieBennett.jpg"
+        },
+        "media": {
+            "x2": "assets/img/x2/cannibaleroyale.jpg",
+            "x3": "assets/img/x3/cannibaleroyale.jpg"
+        }
+    },
+    {
+        "id": "ChIJ3UYLU3XixUcRlxW4bSCFqdQ",
+        "label": "Foodhallen",
+        "website": "http://www.foodhallen.nl",
+        "category": "Restaurants",
+        "content": [
+            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim."
+        ],
+        "author": {
+            "name": "Marie Bennett",
+            "role": "Architect",
+            "picture": "assets/img/author/MarieBennett.jpg"
+        },
+        "media": {
+            "x2": "assets/img/x2/bloemenmarkt.jpg",
+            "x3": "assets/img/x3/bloemenmarkt.jpg"
+        }
+    },
+    {
         "id": "ChIJSRE-IcUJxkcRCltjPmVdmtQ",
         "label": "Anne Frank Huis",
         "category": "Typical Amsterdam",
@@ -38,12 +74,12 @@ angular.module('App').constant('places', [
         }
     },
     {
-        "id": "Ei5SdXlzZGFlbGthZGUgMTQ5LCAxMDcyIEFSIEFtc3RlcmRhbSwgTmVkZXJsYW5k",
-        "label": "Cannibale Royale",
-        "website": "http://www.cannibaleroyale.nl",
-        "category": "Restaurants",
+        "id": "ChIJezIPCY8JxkcRgdqv8FrYp8U",
+        "label": "Juice Brothers",
+        "website": "http://www.juicebro.com",
+        "category": "Healthy Bars",
         "content": [
-            "Cannibale Royale, brasserie extraordinaire fulfills all needs for anyone who loves good food, lots of meat, exotic beers and the most beautiful wines."
+            "JuiceBrothers their goal is to create quality cold-pressed juice that is nutritious and delicious as hell."
         ],
         "author": {
             "name": "Marie Bennett",
@@ -51,44 +87,8 @@ angular.module('App').constant('places', [
             "picture": "assets/img/author/MarieBennett.jpg"
         },
         "media": {
-            "x2": "assets/img/x2/cannibaleroyale.jpg",
-            "x3": "assets/img/x3/cannibaleroyale.jpg"
-        }
-    },
-    {
-        "id": "ChIJ3UYLU3XixUcRlxW4bSCFqdQ",
-        "label": "Foodhallen",
-        "website": "http://www.foodhallen.nl",
-        "category": "Restaurants",
-        "content": [
-            "Esse molestie consequat vel illum dolore eu feugiat. Modo typi qui nunc nobis videntur parum clari fiant sollemnes in. Ut laoreet dolore magna aliquam erat volutpat ut wisi enim. Per seacula quarta decima et quinta decima eodem. In iis qui facit eorum, claritatem Investigationes demonstraverunt lectores legere me. Eleifend option congue nihil imperdiet doming id quod mazim."
-        ],
-        "author": {
-            "name": "Marie Bennett",
-            "role": "Architect",
-            "picture": "assets/img/author/MarieBennett.jpg"
-        },
-        "media": {
-            "x2": "assets/img/x2/bloemenmarkt.jpg",
-            "x3": "assets/img/x3/bloemenmarkt.jpg"
-        }
-    },
-    {
-        "id": "ChIJIyPEUu8JxkcRTmRcuMxC9yc",
-        "label": "Hi-Tec Store",
-        "website": "http://www.hi-tec.amsterdam",
-        "content": [
-            "Dolore te feugait nulla facilisi nam liber tempor cum, soluta nobis eleifend option. Facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod.",
-            "Sequitur mutationem consuetudium lectorum mirum est notare quam littera gothica quam nunc? Qui eodem modo; typi qui nunc nobis videntur parum clari fiant sollemnes in?"
-        ],
-        "author": {
-            "name": "Andrea Clarke",
-            "role": "Entrepreneur",
-            "picture": "assets/img/author/AndreaClarke.jpg"
-        },
-        "media": {
-            "x2": "assets/img/x2/hitecstore.jpg",
-            "x3": "assets/img/x3/hitecstore.jpg"
+            "x2": "assets/img/x2/juicebrothers.jpg",
+            "x3": "assets/img/x3/juicebrothers.jpg"
         }
     },
     {
@@ -111,21 +111,21 @@ angular.module('App').constant('places', [
         }
     },
     {
-        "id": "ChIJezIPCY8JxkcRgdqv8FrYp8U",
-        "label": "Juice Brothers",
-        "website": "http://www.juicebro.com",
-        "category": "Healthy Bars",
+        "id": "ChIJIyPEUu8JxkcRTmRcuMxC9yc",
+        "label": "Hi-Tec Store",
+        "website": "http://www.hi-tec.amsterdam",
         "content": [
-            "JuiceBrothers their goal is to create quality cold-pressed juice that is nutritious and delicious as hell."
+            "Dolore te feugait nulla facilisi nam liber tempor cum, soluta nobis eleifend option. Facit eorum claritatem Investigationes demonstraverunt lectores legere me lius quod.",
+            "Sequitur mutationem consuetudium lectorum mirum est notare quam littera gothica quam nunc? Qui eodem modo; typi qui nunc nobis videntur parum clari fiant sollemnes in?"
         ],
         "author": {
-            "name": "Marie Bennett",
-            "role": "Architect",
-            "picture": "assets/img/author/avatars/MarieBennett.jpg"
+            "name": "Andrea Clarke",
+            "role": "Entrepreneur",
+            "picture": "assets/img/author/AndreaClarke.jpg"
         },
         "media": {
-            "x2": "assets/img/x2/juicebrothers.jpg",
-            "x3": "assets/img/x3/juicebrothers.jpg"
+            "x2": "assets/img/x2/hitecstore.jpg",
+            "x3": "assets/img/x3/hitecstore.jpg"
         }
     },
     {
@@ -134,8 +134,7 @@ angular.module('App').constant('places', [
         "category": "Typical Amsterdam",
         "content": [
             "Iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla. Option congue nihil imperdiet doming id quod mazim placerat facer possim.",
-            "Minim veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea! Facilisi nam liber tempor cum soluta nobis eleifend assum typi non habent claritatem insitam est.",
-            "Ea commodo consequat duis autem vel eum iriure dolor in hendrerit? Litterarum formas humanitatis per seacula quarta decima et quinta decima eodem modo typi."
+            "Minim veniam quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea! Facilisi nam liber tempor cum soluta nobis eleifend assum typi non habent claritatem insitam est."
         ],
         "author": {
             "name": "Shannon Banks",
