810e98058c77572a8501c5d3f8202123484ac40e
Natan Santolo <natan.santolo@gmail.com>
2016-08-08 16:03:55 +0200

    Hacking printing mode, to fit salari quirks.

--- src/StaticMapController.js
+++ src/StaticMapController.js
@@ -1,8 +1,9 @@
 angular
   .module('App')
   .controller('StaticMapController', [
-    '$scope', '$rootScope', '$window',
+    '$scope', '$rootScope', '$window', '$timeout',
     'appModel', 'mapModel',
+    'mapService', 'directionsService',
     StaticMapController
   ]);
 
@@ -12,20 +13,66 @@ angular
  * @param avatarsService
  * @constructor
  */
-function StaticMapController($scope, $rootScope, $window, appModel, mapModel) {
+function StaticMapController($scope, $rootScope, $window, $timeout, appModel, mapModel, mapService, directionsService) {
 
   if (!google || !google.maps) {
     console.error('Google Maps API is unavailable.');
   }
 
+  function resizeMap() {
+    $timeout(triggerResize, 1);
+    $timeout(mapService.getReady, 2);
+  }
+
+  function donePrinting() {
+    angular.element(document.getElementById('map-container')).removeClass('printing-size');
+    $timeout(resizeMap, 10);
+  }
+
+  function triggerResize() {
+    window.dispatchEvent(new Event('resize'));
+  }
+
   var self = this;
+
+  // self.staticMapURL = directionsService.getStaticMapWithDirections();
 
   self.cancel = function() {
     $rootScope.$emit(appModel.events.prevState);
   };
 
   self.print = function() {
-    $window.print();
+    angular.element(document.getElementById('map-container')).addClass('printing-size');
+    $timeout(resizeMap, 10);
+    $timeout($window.print, 100);
   };
+
+  $scope.$on('$destroy', function iVeBeenDismissed() {
+    window.onbeforeprint = null;
+    window.onafterprint = null;
+  });
+
+  (function() {
+    var beforePrint = function() {
+      // console.log('Functionality to run before printing.');
+    };
+    var afterPrint = function() {
+      $timeout(donePrinting, 100);
+    };
+
+    if (window.matchMedia) {
+      var mediaQueryList = window.matchMedia('print');
+      mediaQueryList.addListener(function(mql) {
+        if (mql.matches) {
+          beforePrint();
+        } else {
+          afterPrint();
+        }
+      });
+    }
+
+    window.onbeforeprint = beforePrint;
+    window.onafterprint = afterPrint;
+  }());
 
 }
